79a3cc6e730e4e0925e325b477d8f7b4d60120cb
kinu <kinu.mellowlife@gmail.com>
2019-07-01 21:31:05 +0900

    add image extension

--- trunk/Milky/Milky/Extensions/ArrayExtension.cs
+++ trunk/Milky/Milky/Extensions/ArrayExtension.cs
@@ -36,6 +36,30 @@ namespace Milky.Extensions
 		/// <param name="format">書式</param>
 		/// <param name="separator">１バイトごとのセパレータ</param>
 		/// <returns>変換後の文字列</returns>
+		public static string ToString<T>(this T[] array, Func<int,T,string> convert, string separator = "")
+		{
+			string ret = "";
+			if (array == null)
+				return "";
+			array.ForAll((index, data) =>
+			{
+				ret += convert(index, data);
+
+				if (index < (array.Length - 1))
+				{
+					ret += separator;
+				}
+			});
+			return ret;
+		}
+
+		/// <summary>
+		/// 配列を文字列に変換する
+		/// </summary>
+		/// <param name="array">配列</param>
+		/// <param name="format">書式</param>
+		/// <param name="separator">１バイトごとのセパレータ</param>
+		/// <returns>変換後の文字列</returns>
 		public static string ToString<T>(this IEnumerable<T> list, string format, string separator = "")
 		{
 			string ret = "";
@@ -51,46 +75,91 @@ namespace Milky.Extensions
 			return ret;
 		}
 
+		/// <summary>
+		/// reverse array
+		/// </summary>
+		/// <typeparam name="T">type of array</typeparam>
+		/// <param name="array">target array</param>
 		public static void Reverse<T>(this T[] array)
 		{
 			Array.Reverse(array);
 		}
 
+		/// <summary>
+		/// convert to byte array
+		/// </summary>
+		/// <param name="value">convert from value</param>
+		/// <returns>byte array</returns>
 		public static byte[] ToBytes(this byte value)
 		{
 			return new byte[] { value };
 		}
 
+		/// <summary>
+		/// convert to byte array
+		/// </summary>
+		/// <param name="value">convert from value</param>
+		/// <returns>byte array</returns>
 		public static byte[] ToBytes(this short value)
 		{
 			return BitConverter.GetBytes(value);
 		}
 
+		/// <summary>
+		/// convert to byte array
+		/// </summary>
+		/// <param name="value">convert from value</param>
+		/// <returns>byte array</returns>
 		public static byte[] ToBytes(this int value)
 		{
 			return BitConverter.GetBytes(value);
 		}
 
+		/// <summary>
+		/// convert to byte array
+		/// </summary>
+		/// <param name="value">convert from value</param>
+		/// <returns>byte array</returns>
 		public static byte[] ToBytes(this long value)
 		{
 			return BitConverter.GetBytes(value);
 		}
 
+		/// <summary>
+		/// convert to byte array
+		/// </summary>
+		/// <param name="value">convert from value</param>
+		/// <returns>byte array</returns>
 		public static byte[] ToBytes(this ushort value)
 		{
 			return BitConverter.GetBytes(value);
 		}
 
+		/// <summary>
+		/// convert to byte array
+		/// </summary>
+		/// <param name="value">convert from value</param>
+		/// <returns>byte array</returns>
 		public static byte[] ToBytes(this uint value)
 		{
 			return BitConverter.GetBytes(value);
 		}
 
+		/// <summary>
+		/// convert to byte array
+		/// </summary>
+		/// <param name="value">convert from value</param>
+		/// <returns>byte array</returns>
 		public static byte[] ToBytes(this ulong value)
 		{
 			return BitConverter.GetBytes(value);
 		}
 
+		/// <summary>
+		/// convert to byte array
+		/// </summary>
+		/// <param name="value">convert from value</param>
+		/// <returns>byte array</returns>
 		public static byte GetByte(this byte[] array)
 		{
 			if (array.Length < 1)
@@ -98,6 +167,11 @@ namespace Milky.Extensions
 			return array[0];
 		}
 
+		/// <summary>
+		/// convert to byte array
+		/// </summary>
+		/// <param name="value">convert from value</param>
+		/// <returns>byte array</returns>
 		public static short GetShort(this byte[] array)
 		{
 			if (array.Length < 2)
@@ -105,6 +179,11 @@ namespace Milky.Extensions
 			return BitConverter.ToInt16(array, 0);
 		}
 
+		/// <summary>
+		/// convert to byte array
+		/// </summary>
+		/// <param name="value">convert from value</param>
+		/// <returns>byte array</returns>
 		public static int GetInt(this byte[] array)
 		{
 			if (array.Length < 4)
@@ -112,6 +191,11 @@ namespace Milky.Extensions
 			return BitConverter.ToInt32(array, 0);
 		}
 
+		/// <summary>
+		/// convert to byte array
+		/// </summary>
+		/// <param name="value">convert from value</param>
+		/// <returns>byte array</returns>
 		public static long GetLong(this byte[] array)
 		{
 			if (array.Length < 8)
@@ -119,6 +203,11 @@ namespace Milky.Extensions
 			return BitConverter.ToInt64(array, 0);
 		}
 
+		/// <summary>
+		/// convert to byte array
+		/// </summary>
+		/// <param name="value">convert from value</param>
+		/// <returns>byte array</returns>
 		public static ushort GetUShort(this byte[] array)
 		{
 			if (array.Length < 2)
@@ -126,6 +215,11 @@ namespace Milky.Extensions
 			return BitConverter.ToUInt16(array, 0);
 		}
 
+		/// <summary>
+		/// convert to byte array
+		/// </summary>
+		/// <param name="value">convert from value</param>
+		/// <returns>byte array</returns>
 		public static uint GetUInt(this byte[] array)
 		{
 			if (array.Length < 4)
@@ -133,6 +227,11 @@ namespace Milky.Extensions
 			return BitConverter.ToUInt32(array, 0);
 		}
 
+		/// <summary>
+		/// convert to byte array
+		/// </summary>
+		/// <param name="value">convert from value</param>
+		/// <returns>byte array</returns>
 		public static ulong GetULong(this byte[] array)
 		{
 			if (array.Length < 8)
---
+++ trunk/Milky/Milky/Extensions/ImageExtension.cs
@@ -0,0 +1,79 @@
+﻿using System;
+using System.Drawing;
+using System.Drawing.Imaging;
+using System.Runtime.InteropServices;
+
+namespace Milky.Extensions
+{
+	/// <summary>
+	/// 画像関連の拡張メソッド
+	/// </summary>
+	public static class ImageExtension
+	{
+		/// <summary>
+		/// １ピクセルを何バイトで表現するか
+		/// </summary>
+		/// <param name="image">画像情報</param>
+		/// <returns>バイト長</returns>
+		public static int PixelSize(this Bitmap image)
+		{
+			return Bitmap.GetPixelFormatSize(image.PixelFormat) / 8;
+		}
+
+		/// <summary>
+		/// 指定画像に対し、ビット情報としてのインデックス番号からピクセル位置に変換する。
+		/// GetBytes()で取得したバイト情報を捜査する際に座標に戻す場面を想定
+		/// </summary>
+		/// <param name="image">基準画像</param>
+		/// <param name="index">ビット位置</param>
+		/// <returns>ピクセル座標</returns>
+		public static Point ToPixelXY(this Bitmap image, int index)
+		{
+			return new Point(
+				(index / image.PixelSize()) % image.Width,
+				(index / image.PixelSize()) / image.Width
+				);
+		}
+
+		/// <summary>
+		/// 指定画像のビット情報をバイト配列として取得
+		/// </summary>
+		/// <param name="image">元画像</param>
+		/// <param name="rect">取得したい範囲</param>
+		/// <returns>ビット情報</returns>
+		public static byte[] GetBytes(this Bitmap image, Rectangle rect)
+		{
+			//ロック
+			BitmapData bmpData = image.LockBits(rect, ImageLockMode.ReadWrite, image.PixelFormat);
+
+			//ビットマップの先頭アドレス
+			IntPtr ptr = bmpData.Scan0;
+
+			//必要なバイト数
+			int length = image.Width * image.Height * image.PixelSize();
+
+			//格納先
+			byte[] data = new byte[length];
+
+			//コピー
+			Marshal.Copy(ptr, data, 0, length);
+
+			//ロック解除
+			image.UnlockBits(bmpData);
+
+			return data;
+		}
+
+		/// <summary>
+		/// 指定画像のビット情報をバイト配列として取得
+		/// </summary>
+		/// <param name="image">取得する画像</param>
+		/// <returns>ビット情報</returns>
+		public static byte[] GetBytes(this Bitmap image)
+		{
+			Rectangle rect = new Rectangle(0, 0, image.Width, image.Height);
+
+			return image.GetBytes(rect);
+		}
+	}
+}
